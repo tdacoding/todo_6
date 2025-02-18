8f0b6b83cb0e439398fe36de53b5445eb0676cc0
Dmitry Timushev <timushev@gmail.com>
2025-02-18 11:41:46 +0300

    init new react-vite template

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import styles from './App.module.css';
+import { createElement } from 'react';
+
+function App() {
+	return (
+		<div className={styles.app}>
+			<h1>edede</h1>
+			<header className={styles.appHeader}>Заголовок</header>
+		</div>
+	);
+}
+
+export default App;
